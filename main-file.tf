ca73a2a5c4340693350efcd2b67d380351b18f49
Sathish <sathishdevopscloud@gmail.com>
2023-10-05 07:57:03 +0530

    Terraform variables class

--- main-file.tf
+++ main-file.tf
@@ -1,14 +1,24 @@
 resource "local_file" "simple_text" {
   filename = var.filename1
-  content  = var.content1
+  content  = data.local_file.mycontent.content
+
+  depends_on = [
+    local_file.second_text
+  ]
 }
 
 resource "local_file" "second_text" {
-  filename = "${random_id.rand_id.hex}.txt"
-  content  = "${var.content2} - ${random_pet.my-pet-name1.id}"
+  filename = "file2.txt"
+  content  = "content changes 123123123 "
+  #lifecycle {
+  #  ignore_changes = [
+  #    content
+  #  ]
+  #  prevent_destroy = true
+  #}
 
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
+data "local_file" "mycontent"{
+  filename = ".\\my-reference.txt"
+}
